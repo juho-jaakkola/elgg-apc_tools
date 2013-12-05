28cee75dcc1cc0c76feb0f5ac051139b987031dc
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2013-12-05 10:33:53 +0200

    Added finnish translations.

---
+++ languages/fi.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang = array(
+	'apc_tools:apc' => 'APC',
+	'apc_tools:flush' => 'Tyhjennä APC',
+	'apc_tools:flush:success' => 'Tyhjennettiin APC opcode cache',
+	'apc_tools:flush:fail' => 'APC opcode cachen tyhjentäminen epäonnistui',
+	'apc_tools:apc:not_available' => 'Näyttää siltä, että APC ei ole asennettuna tälle palvelimelle.',
+
+	'admin:statistics:apc' => 'APC',
+);
+
+add_translation('fi', $lang);
